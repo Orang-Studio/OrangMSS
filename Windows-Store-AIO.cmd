7eabd105d0ea4b3fae0d08926271338cd1e7868a
Adas <100019532+adasjusk@users.noreply.github.com>
2025-03-06 08:32:51 +0200

    Update Windows-Store-AIO.cmd

--- Windows-Store-AIO.cmd
+++ Windows-Store-AIO.cmd
@@ -32,20 +32,14 @@ bitsadmin /transfer dependency /download /priority normal https://raw.githubuser
 bitsadmin /transfer dependency /download /priority normal https://raw.githubusercontent.com/kkkgo/LTSC-Add-MicrosoftStore/master/Microsoft.DesktopAppInstaller_1.6.29000.1000_neutral_~_8wekyb3d8bbwe.AppxBundle %USERPROFILE%\AppData\Local\Temp\Microsoft.DesktopAppInstaller_1.6.29000.1000_neutral_~_8wekyb3d8bbwe.AppxBundle
 ::1
 bitsadmin /transfer Installer /download /priority normal https://raw.githubusercontent.com/kkkgo/LTSC-Add-MicrosoftStore/master/Add-Store.cmd %USERPROFILE%\AppData\Local\Temp\store.bat
-cls
-
 if "%2"=="firstrun" exit
 cmd /c "%0" null firstrun
-
 if "%1"=="skipuac" goto skipuacstart
-
 :checkPrivileges
 NET FILE 1>NUL 2>NUL
 if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
-
 :getPrivileges
 if '%1'=='ELEV' (shift & goto gotPrivileges)
-
 setlocal DisableDelayedExpansion
 set "batchPath=%~0"
 setlocal EnableDelayedExpansion
@@ -53,16 +47,12 @@ ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\s.vbs"
 ECHO UAC.ShellExecute "!batchPath!", "ELEV", "", "runas", 1 >> "%temp%\s.vbs"
 "%temp%\s.vbs"
 exit /B
-
 :gotPrivileges
 setlocal & pushd .
-
 cd /d %~dp0
 cmd /c "%0" skipuac firstrun
 cd /d %~dp0
-
 :skipuacstart
-
 if "%2"=="firstrun" exit
 
 cmd /k %USERPROFILE%\AppData\Local\Temp\store.bat
